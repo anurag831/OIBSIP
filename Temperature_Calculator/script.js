e7a7263793fc0ff4612999753b6d19ac561cbef9
Anurag <anuragganguly83@gmail.com>
2024-04-23 23:10:28 +0530

    50% done

---
+++ Temperature_Calculator/script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  let degrees = document.getElementById("degree").value;
+  console.log(degrees);
+  let type = document.getElementById("type1").value;
+  console.log(type);
+});
