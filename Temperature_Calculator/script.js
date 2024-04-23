92c0637f542e469c5da07a5a7b9a145af99d7d8a
Anurag <anuragganguly83@gmail.com>
2024-04-23 23:40:15 +0530

    Complete

--- Temperature_Calculator/script.js
+++ Temperature_Calculator/script.js
@@ -4,4 +4,13 @@ btn.addEventListener("click", () => {
   console.log(degrees);
   let type = document.getElementById("type1").value;
   console.log(type);
+  let output = document.getElementById('output');
+  let deg2;
+  if (type == "celcius") {
+    deg2 = (degrees * 9.0) / 5.0 + 32.0;
+    output.innerHTML = `${deg2} °F`
+  } else {
+    deg2 = (degrees - 32) * 5/9;
+    output.innerHTML = `${deg2} °C`
+  }
 });
